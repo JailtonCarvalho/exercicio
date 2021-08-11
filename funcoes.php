1f81f9ba3d77b9e93a6052148a832cd20856866d
Jailton Carvalho <jailtons.c@hotmail.com>
2021-08-11 13:01:44 -0300

    Reestruturação do código em funções

--- funcoes.php
+++ funcoes.php
@@ -45,4 +45,64 @@ function lerSaldoContaPoupanca(){
     return file_get_contents('saldo_cp.txt');
 }
 
+function pegaValor($operacao){
+    switch ($operacao) {
+        case 'saque':
+            $valor = $_POST['valor_saque'];
+            return $valor;
+            break;
+        case 'deposito':
+            $valor = $_POST['valor_deposito'];
+            return $valor;
+            break;
+        default:
+            throw new Exception("Error Processing Request", 1);
+            break;
+    }
+}
+function executarOperacao($opcao, $operacao, $valor, $saldo_poupanca, $saldo_corrente){
+    // Informações para operação em conta poupança.
+    if ($opcao == "poupança"){
+        switch ($operacao) {
+            case 'deposito':
+            depositar($valor, $saldo_poupanca, $opcao);
+                break;
+            case 'saque':
+                sacar($saldo_poupanca, $valor, $opcao);
+                break;
+            default:
+                throw new Exception("Error Processing Request", 1);
+                break;
+        }
+
+    // Informações para operação em conta corrente.
+    } elseif ($opcao == "corrente") {
+        switch ($operacao) {
+            case 'deposito':
+                depositar($valor, $saldo_corrente, $opcao);
+                break;
+                case 'saque':
+                    sacar($saldo_corrente, $valor, $opcao);
+                    break;
+                    default:
+                    throw new Exception("Error Processing Request", 1);
+                    break;
+                }            
+            } else {
+                echo "selecione o tipo de conta";
+            }
+        }
+        
+        function app(){
+            //pegar variáveis necessárias
+            $opcao = $_POST['opcao'];
+            $operacao = $_POST['operacao'];
+            $saldo_corrente = lerSaldoContaCorrente();
+            $saldo_poupanca = lerSaldoContaPoupanca();
+            //pegar valor por operação
+            $valor = pegaValor($operacao);
+            //direcionar a operação conforme a opção
+            executarOperacao($opcao, $operacao, $valor, $saldo_poupanca, $saldo_corrente);
+}
+
 ?>
--- receber-form.php
+++ receber-form.php
@@ -1,62 +1,6 @@
 <?php
 require ('funcoes.php');
 
-$saldo_corrente = lerSaldoContaCorrente();
-$saldo_poupanca = lerSaldoContaPoupanca();
-
-
-$opcao = $_POST['opcao'];
-$operacao = $_POST['operacao'];
-
-switch ($operacao) {
-    case 'saque':
-        $valor_saque = $_POST['valor_saque'];
-        break;
-    case 'deposito':
-        $valor_deposito = $_POST['valor_deposito'];
-        break;
-    default:
-        throw new Exception("Error Processing Request", 1);
-        
-        break;
-}
-
-
-// Informações para operação em conta poupança.
-    if ($opcao == "poupança"){
-        switch ($operacao) {
-            case 'deposito':
-               depositar($valor_deposito, $saldo_poupanca, $opcao);
-                break;
-            case 'saque':
-                sacar($saldo_poupanca, $valor_saque, $opcao);
-                break;
-            default:
-                throw new Exception("Error Processing Request", 1);
-            
-                break;
-        }
-    
-
-// Informações para operação em conta corrente.
-    } elseif ($opcao == "corrente") {
-        switch ($operacao) {
-            case 'deposito':
-               depositar($valor_deposito, $saldo_corrente, $opcao);
-                break;
-            case 'saque':
-                sacar($saldo_corrente, $valor_saque, $opcao);
-                break;
-            default:
-                throw new Exception("Error Processing Request", 1);
-            
-                break;
-        }
-        
-        
-        
-    } else {
-        echo "selecione o tipo de conta";
-    }
+app();
 
 ?>
